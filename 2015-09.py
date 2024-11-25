25736950ad7c917a2fecfe7fd33c8a1ce8d32b6b
Luca Bellanti <luca.bellanti@gmail.com>
2024-11-26 00:08:50 +0100

    2015 day 9

--- 2015-09.py
+++ 2015-09.py
@@ -8,6 +8,9 @@ from codetiming import Timer
 from icecream import ic
 from pprint import pprint
 
+import networkx as nx
+from itertools import permutations
+
 import utils
 
 # YEAR and DAY from the current file name YYYY-DD.
@@ -46,18 +49,42 @@ def get_input() -> any:
 @Timer(name="Parsing", text="Parsing.....DONE: {milliseconds:.0f} ms")
 def parsing_input(data) -> any:
     """
-    We'll parse the input line by line.
+    We'll do something with the input
     """
+    G = nx.Graph()
+    for line in data:
+        from_, _, to, _, dist = line.split()
+        G.add_edge(from_, to, weight=int(dist))
+
+    # create a dict with all the distances between two cities
+    cities_distance = {}
+    for edge in G.edges:
+        cities_distance[edge] = G.get_edge_data(edge[0], edge[1])['weight']
+        cities_distance[edge[::-1]] = G.get_edge_data(edge[0], edge[1])['weight']
     
-    return data
+    # compute all the paths, save the distance and the route
+    cities = list(G.nodes)
+    all_routes = permutations(cities)
+
+    distances = {}
+    for route in all_routes:
+        # find the distance for every route
+        distance = 0
+        for i in range(len(route) - 1):
+            distance += cities_distance[(route[i], route[i+1])]
+        distances[distance] = route
+
+    # order distances by int(key)
+    distances = dict(sorted(distances.items(), key=lambda item: item[0]))
+
+    return distances
 
 
 # Part 1
 @Timer(name="Part 1", text="Part 1......DONE: {milliseconds:.0f} ms")
 def part1(data: any) -> int:
     sol1 = 0
-    for string in data:
-        sol1 += len(string) - len(eval(string))
+    sol1 = list(data.keys())[0]
     return sol1
 
 
@@ -65,9 +92,7 @@ def part1(data: any) -> int:
 @Timer(name="Part 2", text="Part 2......DONE: {milliseconds:.0f} ms")
 def part2(data: any) -> int:
     sol2 = 0
-    for string in data:
-        enc_string = '"' + string.replace('\\', '\\\\').replace('"', '\\"') + '"'
-        sol2 += len(enc_string) - len(string)
+    sol2 = list(data.keys())[-1]
     return sol2
 
 data = get_input()
