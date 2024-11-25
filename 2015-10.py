ee040ff71711766d0c2daacd34ed82eae60c82e6
Luca Bellanti <luca.bellanti@gmail.com>
2024-11-26 00:52:27 +0100

    2015 day 10

---
+++ 2015-10.py
@@ -0,0 +1,102 @@
+import os
+
+# from pprint import pprint as pp
+# from datetime import date
+
+from codetiming import Timer
+# from dataclassy import dataclass
+from icecream import ic
+from pprint import pp
+import re
+
+import networkx as nx
+from itertools import permutations
+
+import utils
+
+# YEAR and DAY from the current file name YYYY-DD.
+YEAR = int(os.path.basename(__file__).split(".")[0].split("-")[0])
+DAY = int(os.path.basename(__file__).split(".")[0].split("-")[1])
+
+# Used to overwrite the year and day
+# YEAR = 2015
+# DAY = 07
+
+EXAMPLE = False
+INFO = True
+DEBUG = True
+
+if DEBUG:
+    ic.enable()
+else:
+    ic.disable()
+
+
+def pprint(data: any) -> None:
+    if INFO:
+        print(data)
+
+# Input parsing
+print()
+@Timer(name="Opening", text="Opening.....DONE: {milliseconds:.0f} ms")
+def get_input() -> any:
+    """
+    Get the input from the file or internet
+    """
+    data = utils.get_data(YEAR, DAY, strip=True, example=EXAMPLE)
+    return data
+
+
+@Timer(name="Parsing", text="Parsing.....DONE: {milliseconds:.0f} ms")
+def parsing_input(data) -> any:
+    """
+    We'll do something with the input
+    """
+    data = data[0]
+    return data
+
+def look_and_say(stringa: str) -> str:
+    # use the regex /(\d)\1*/g and iterate the matches
+    matches = re.finditer(r"(\d)\1*", stringa)
+
+    newstring = ""
+
+    for match in matches:
+        match = match.group()
+        newstring += str(len(match)) + match[0]
+
+    return newstring
+
+    
+# Part 1
+@Timer(name="Part 1", text="Part 1......DONE: {milliseconds:.0f} ms")
+def part1(data: any) -> int:
+    sol1 = 0
+    stringa = data
+
+    for _ in range(40):
+        stringa = look_and_say(stringa)
+    sol1 = len(stringa)
+    return sol1
+
+
+# Part 2
+@Timer(name="Part 2", text="Part 2......DONE: {milliseconds:.0f} ms")
+def part2(data: any) -> int:
+    sol2 = 0
+    stringa = data
+
+    for _ in range(50):
+        stringa = look_and_say(stringa)
+    sol2 = len(stringa)
+    return sol2
+
+data = get_input()
+data = parsing_input(data)
+s1 = part1(data)
+s2 = part2(data)
+
+print()
+print(f"=========:: [DAY {DAY}] ::=========")
+print(f"Soluzione Parte 1: [{s1}]")
+print(f"Soluzione Parte 2: [{s2}]")
